601062a8af5a9a834e8e5f651cb425f17c2986ff
G. Subham Kumar Patra <96821893+gsubhampatra@users.noreply.github.com>
2024-01-13 01:01:52 +0530

    Update cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ import cookieParser from "cookie-parser"
 // import adminRoute from "./routes/admin.route.js"
 app.use(express.json())
 app.use(cors({
-    origin:"*"
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
 }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
