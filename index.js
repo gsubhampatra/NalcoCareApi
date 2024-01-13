7f7b6a140a935e91dff01c93863557035d0252c1
G. Subham Kumar Patra <96821893+gsubhampatra@users.noreply.github.com>
2024-01-13 09:07:24 +0000

    cors fix

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send({ success: true, message: "Welcome to NALCO Care API" });
