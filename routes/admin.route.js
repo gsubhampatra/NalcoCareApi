dd0ecde4a157366602c410b952ee48151445a9e1
G. Subham Kumar Patra <96821893+gsubhampatra@users.noreply.github.com>
2024-01-24 23:18:09 +0530

    Update admin.route.js

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router
   .route("/get-all-appointments")
-  .get(verifyToken, isAdmin, getAllAppointments);
+  .get(verifyToken, getAllAppointments);
 
 router.route("/delete-doctor/:id").delete(verifyToken, isAdmin, deleteDoctor);
 
