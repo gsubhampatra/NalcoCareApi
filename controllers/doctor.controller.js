2677eef217327859f68c2ae1ec199166289faf29
G. Subham Kumar Patra <96821893+gsubhampatra@users.noreply.github.com>
2024-01-28 06:58:38 +0000

    Refactor doctor and patient controllers

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -2,43 +2,45 @@ import Doctor from "../models/doctor.model.js";
 import Appointment from "../models/appointment.model.js";
 
 const getDoctor = async (req, res) => {
-    try {
-        const doctor = await Doctor.findOne({ email: req.params.id });
-        if (!doctor) {
-            return res.status(400).json({
-                success: false,
-                message: "Doctor not found",
-            });
-        }
-        return res.status(200).json({ success: true, doctor });
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        });
+  try {
+    const doctor = await Doctor.findOne({ email: req.params.id });
+    if (!doctor) {
+      return res.status(400).json({
+        success: false,
+        message: "Doctor not found",
+      });
     }
-}
+    return res.status(200).json({
+      success: true,
+      user: {
+        role: "doctor",
+        ...doctor,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 const getAllDoctors = async (req, res) => {
-    try {
-        const doctors = await Doctor.find();
-        if (!doctors) {
-            return res.status(400).json({
-                success: false,
-                message: "Doctor not found",
-            });
-        }
-        return res.status(200).json({ success: true, doctors });
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        });
+  try {
+    const doctors = await Doctor.find();
+    if (!doctors) {
+      return res.status(400).json({
+        success: false,
+        message: "Doctor not found",
+      });
     }
-}
+    return res.status(200).json({ success: true, doctors });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-
-
-export { getDoctor, getAllDoctors }
+export { getDoctor, getAllDoctors };
--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -1,42 +1,45 @@
 import Patient from "../models/patient.model.js";
 
-
 const getPatient = async (req, res) => {
-    try {
-        const patient = await Patient.findOne({ email: req.params.id });
-        if (!patient) {
-            return res.status(400).json({
-                success: false,
-                message: "Patient not found",
-            });
-        }
-        return res.status(200).json({ success: true, patient });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        });
+  try {
+    const patient = await Patient.findOne({ email: req.params.id });
+    if (!patient) {
+      return res.status(400).json({
+        success: false,
+        message: "Patient not found",
+      });
     }
-}
+    return res.status(200).json({
+      success: true,
+      user: {
+        ...patient,
+        role: "patient",
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 const getAllPatients = async (_, res) => {
-    try {
-        const patients = await Patient.find();
-        if (!patients) {
-            return res.status(400).json({
-                success: false,
-                message: "Patient not found",
-            });
-        }
-        return res.status(200).json({ success: true, patients });
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        });
+  try {
+    const patients = await Patient.find();
+    if (!patients) {
+      return res.status(400).json({
+        success: false,
+        message: "Patient not found",
+      });
     }
-}
+    return res.status(200).json({ success: true, patients });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-
-export { getPatient, getAllPatients }
+export { getPatient, getAllPatients };
